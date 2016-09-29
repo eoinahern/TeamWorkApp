6c44fb70101d255b3da4964920d0a54b1a59368a
eoinahern <eoin.ahern@mycit.ie>
2016-09-29 14:10:58 +0100

    first commit

---
+++ app/src/main/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/TeamWorkApp.java
@@ -0,0 +1,16 @@
+package ie.ahern.eoin.demo.teamworkapp.teamworkapp;
+
+import android.app.Application;
+
+/**
+ * Created by eoin_pc on 29/09/2016.
+ */
+public class TeamWorkApp extends Application {
+
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/presentation/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package ie.ahern.eoin.demo.teamworkapp.teamworkapp.presentation.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.ahern.eoin.demo.teamworkapp.teamworkapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
