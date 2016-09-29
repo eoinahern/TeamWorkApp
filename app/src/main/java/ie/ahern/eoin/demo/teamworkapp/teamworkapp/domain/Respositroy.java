d4651eb4a9f1e0cdaa0934b1e141b897a9ff3eaa
eoinahern <eoin.ahern@mycit.ie>
2016-09-29 15:17:08 +0100

    added usecase repositiory

---
+++ app/src/main/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/domain/Respositroy.java
@@ -0,0 +1,7 @@
+package ie.ahern.eoin.demo.teamworkapp.teamworkapp.domain;
+
+/**
+ * Created by eoin_pc on 29/09/2016.
+ */
+public interface Respositroy {
+}
---
+++ app/src/main/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/domain/interactor/UseCase.java
@@ -0,0 +1,11 @@
+package ie.ahern.eoin.demo.teamworkapp.teamworkapp.domain.interactor;
+
+/**
+ * Created by eoin_pc on 29/09/2016.
+ */
+public abstract class UseCase {
+
+
+
+
+}
