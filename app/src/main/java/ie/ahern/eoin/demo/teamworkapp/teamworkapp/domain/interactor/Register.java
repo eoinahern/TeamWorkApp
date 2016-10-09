6d225766ae55d4445bdb8ccc34646533e9b01bb5
eoinahern <eoinpahern@yahoo.co.uk>
2016-10-09 18:31:07 +0100

    updated register usecase

--- app/src/main/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/domain/interactor/Register.java
+++ app/src/main/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/domain/interactor/Register.java
@@ -1,15 +1,28 @@
 package ie.ahern.eoin.demo.teamworkapp.teamworkapp.domain.interactor;
 
+import javax.inject.Inject;
+
+import ie.ahern.eoin.demo.teamworkapp.teamworkapp.domain.DataRepository;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 
 /**
  * Created by eoin_pc on 29/09/2016.
  */
-public class Register  extends UseCase {
+public class Register extends UseCase {
+
+
+    private DataRepository repo;
+
+    @Inject
+    public Register(DataRepository repo,Scheduler androidscheduler, Scheduler mainshceduler) {
+        super(androidscheduler, mainshceduler);
+        this.repo = repo;
+    }
 
     @Override
-    public Observable BuildUsecaseObservabel(Subscriber subscriber) {
-        return null;
+    public Observable BuildUsecaseObservable() {
+       return repo.getProjects();
     }
 }
