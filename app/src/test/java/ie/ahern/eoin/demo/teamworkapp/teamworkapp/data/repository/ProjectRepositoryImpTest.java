f1670e10383e277d4f3933d37568dbaefda3ffe8
eoinahern <eoinpahern@yahoo.co.uk>
2016-10-12 19:42:03 +0100

    added some on data layer

---
+++ app/src/test/java/ie/ahern/eoin/demo/teamworkapp/teamworkapp/data/repository/ProjectRepositoryImpTest.java
@@ -0,0 +1,75 @@
+package ie.ahern.eoin.demo.teamworkapp.teamworkapp.data.repository;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import ie.ahern.eoin.demo.teamworkapp.teamworkapp.data.web.RestAPICalls;
+import ie.ahern.eoin.demo.teamworkapp.teamworkapp.domain.model.Project.Project;
+import rx.Observable;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by eoin_a on 12/10/2016.
+ */
+public class ProjectRepositoryImpTest {
+
+
+    private ProjectRepositoryImp projectrepo;
+    @Mock private RestAPICalls restcalls;
+    @Mock private Observable<List<Project>> obs;
+    @Mock private Project mockproj;
+
+
+    @Before
+    public void Setup(){
+
+        MockitoAnnotations.initMocks(this);
+        projectrepo = new ProjectRepositoryImp(restcalls);
+    }
+
+    @Test
+    public void retrieveProjects() throws Exception {
+
+        Mockito.when(restcalls.retrieveProjects()).thenReturn(obs);
+        Observable observ = projectrepo.retrieveProjects();
+        verify(restcalls).retrieveProjects();
+        Assert.assertEquals(observ, obs);
+
+    }
+
+    @Test
+    public void retrieveProject() throws Exception {
+
+        Mockito.when(restcalls.retrieveProject(anyString())).thenReturn(Observable.just(mockproj));
+        Observable<Project> proj = projectrepo.retrieveProject(anyString());
+        verify(restcalls).retrieveProject(anyString());
+
+    }
+
+    @Test
+    public void starProject() throws Exception {
+
+    }
+
+    @Test
+    public void unstarProject() throws Exception {
+
+    }
+
+    @Test
+    public void retrieveStarredProjects() throws Exception {
+
+    }
+
+}
